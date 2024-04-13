c21edb1719cab51c0d8302d712518b6d910a5c0a
DamianMotyl <damian.motyl@outlook.com>
2024-04-13 15:55:42 +0200

    gfhgfh

--- app/src/main/java/com/example/app1/MainActivity2.kt
+++ app/src/main/java/com/example/app1/MainActivity2.kt
@@ -2,10 +2,26 @@ package com.example.app1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.text.DecimalFormat
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        var wynik : Double = 0.0;
+        var TV_wynik =findViewById<TextView>(R.id.TV_wynik);
+        var BTN_wroc = findViewById<Button>(R.id.BTN_wroc);
+        val koszyk = intent.extras;
+
+        if (koszyk != null) {
+            wynik = koszyk.getDouble("wynik");
+        }
+        val DC = DecimalFormat("#.##");
+        TV_wynik.setText("Twoje BMI wynosi: "+DC.format(wynik));
+
+
     }
 }
